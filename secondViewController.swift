241b92d8bf5b24a740799d2745adcdf72490e076
Billy Farroll <watanight@hotmail.co.uk>
2015-11-26 09:50:40 +0000

    Update secondViewController.swift

--- secondViewController.swift
+++ secondViewController.swift
@@ -2,15 +2,15 @@
 
 import UIKit
 
+protocol SecondViewControllerDelegate {
 
+func doSomething()
 
-
+}
 
 class SecondViewController: UIViewController {
     
-    
-    
-    
+    var delegate: SecondViewController?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +18,11 @@ class SecondViewController: UIViewController {
         
     }
     
+    @IBAction func doSomething(sender: AnyObject) {
+        
+        delegate?.doSomething(<#T##sender: AnyObject##AnyObject#>)
+        
+    }
     
 }
 
